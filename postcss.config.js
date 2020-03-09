9c357f8e765c9ff5b2ed0f2b8c779b3d9fce42b2
orange-Cream <58293287+orange-Cream@users.noreply.github.com>
2020-03-09 10:49:18 +0800

    px转为rem优化

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,21 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {},
-    'postcss-pxtorem': {
-      // 根值：默认是37.5，来自于设计稿大小的10分之一
-      rootValue: 37.5, // 根植
-      propList: ['*']
+const {
+  sep
+} = require('path')
+
+module.exports = ({
+  file
+}) => {
+  const rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
     }
   }
 }
