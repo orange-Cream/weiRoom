41fef52260ff603909110d2082d2d2eaec20e902
orange-Cream <58293287+orange-Cream@users.noreply.github.com>
2020-03-09 13:09:03 +0800

    登录功能开发-页面布局

--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -1,13 +1,48 @@
 <template>
-  <div class="page-user-chat">
-    <van-nav-bar left-arrow @click-left="$router.back()" title="登录"></van-nav-bar>
+  <div class="user-login">
+    <van-nav-bar title="登录"></van-nav-bar>
+    <van-cell-group>
+      <van-field
+        v-model="loginForm.mobile"
+        type="text"
+        required
+        label="手机号"
+        placeholder="请输入手机号"
+        clearable
+      />
+      <van-field
+        v-model="loginForm.code"
+        type="password"
+        required
+        clearable
+        label="验证码"
+        placeholder="请输入验证码"
+      >
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div class="login-btn">
+     <van-button round block type="info" size="small" @click="login()">登录</van-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'user-chat'
+  name: 'user-chat',
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang="less">
+ .login-btn{
+     margin: 40px;
+ }
+</style>
