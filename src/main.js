cf812fe27ef1f1a857720fdc3d3b2fac63de304d
orange-Cream <58293287+orange-Cream@users.noreply.github.com>
2020-03-09 10:42:47 +0800

    完整引入 vant组件库

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 完整引入 vant 组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
